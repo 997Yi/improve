85d8e5e90591d09a09e0ba46fb6b0e17c154fc29
Fourteen <406499401@qq.com>
2021-01-18 14:11:35 +0800

    index页面更改导航栏

---
+++ src/main/java/com/gfr/improve/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.gfr.improve.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import springfox.documentation.annotations.ApiIgnore;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class HomeController {
+
+
+}

3e50c83557a0d38cb4f651da1cc4ad4ca24941f6
xxx_ <2012357983@qq.com>
2021-01-18 11:53:40 +0800

    test

---
+++ src/test/java/com/gfr/improve/ImproveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gfr.improve;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ImproveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

ebcfef4f91aa3880ce15c9fb5b9b9efa32b5e2eb
Fourteen <406499401@qq.com>
2021-01-18 17:17:59 +0800

    index.html更改路径

--- src/main/java/com/gfr/improve/controller/HomeController.java
+++ src/main/java/com/gfr/improve/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.gfr.improve.controller;
 
+import io.swagger.annotations.ApiOperation;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -9,6 +10,18 @@ import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class HomeController {
+    //    跳转到课程详情页面
+    @ApiIgnore
+    @RequestMapping("getCourseDetail")
+    public String getCourseDetail() {
+        return "courseDetail";
+    }
 
+    //跳转到上传课程页面
+    @ApiIgnore
+    @RequestMapping("getUploadCourse")
+    public String getUploadCourse() {
+        return "uploadCourse";
+    }
 
 }

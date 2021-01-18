1eeef78863c6fd05baf3d037f9928ed12f17b45e
xxx_ <2012357983@qq.com>
2021-01-18 14:00:37 +0800

    主页面+layui

--- src/main/java/com/gfr/improve/ImproveApplication.java
+++ src/main/java/com/gfr/improve/ImproveApplication.java
@@ -1,9 +1,13 @@
 package com.gfr.improve;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@MapperScan("com.gfr.improve.dao")
+@EnableTransactionManagement
 public class ImproveApplication {
 
     public static void main(String[] args) {
